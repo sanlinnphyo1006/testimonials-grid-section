d248084b4d302b02e7612d27e51f23bb0e581f55
sanlinnphyo1006 <sanlinnphyo95@gmail.com>
2023-04-29 11:02:14 +0630

    ADDED README.md

---
+++ README.md
@@ -0,0 +1,65 @@
+# Frontend Mentor - Testimonials grid section solution
+
+This is a solution to the [Testimonials grid section challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/testimonials-grid-section-Nnw6J7Un7). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+
+### Screenshot
+
+For mobiles
+
+![](./screenshots/screenshot-sm.png)
+
+For tablets
+
+![](./screenshots/screenshot-md.png)
+
+For laptops
+
+![](./screenshots/screenshot-lg.png)
+
+### Links
+
+- Solution URL: https://github.com/sanlinnphyo1006/testimonials-grid-section
+- Live Site URL: https://slpprojects-testimonials-grid-section.netlify.app/
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Tailwindcss](https://tailwindcss.com/) - For styles
+
+### What I learned
+
+I learned how to use CSS grid.
+
+### Useful resources
+
+- [CSS Grid Crash Course](https://www.youtube.com/watch?v=0xMQfnTU6oo&t=5s) - This helped me in learning CSS grid.
+
+## Author
+
+- Github - [San Linn Phyo](https://github.com/sanlinnphyo1006)
+- Frontend Mentor - [@sanlinnphyo1006](https://www.frontendmentor.io/profile/sanlinnphyo1006)
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,20 +1,26 @@
 import PropTypes from "prop-types";
 
+import daniel from "../assets/image-daniel.jpg";
+import jeanette from "../assets/image-jeanette.jpg";
+import jonathan from "../assets/image-jonathan.jpg";
+import kira from "../assets/image-kira.jpg";
+import patrick from "../assets/image-patrick.jpg";
 import quotation from "../assets/bg-pattern-quotation.svg";
 
 const Testimonial = ({
   id,
   name,
   role,
-  image,
   title,
   description,
   backgroundColor,
   color,
 }) => {
   const isDaniel = name === "Daniel Clifford";
-  const isPatrick = name === "Patrick Abrams";
+  const isJeanette = name === "Jeanette Harmon";
+  const isJonathan = name === "Jonathan Walters";
   const isKira = name === "Kira Whittle";
+  const isPatrick = name === "Patrick Abrams";
   return (
     <div
       className={`px-7 py-6 rounded-md shadow-md ${
@@ -36,7 +42,19 @@ const Testimonial = ({
         <div className="border-2 rounded-full w-8 h-8">
           <img
             className="rounded-full w-full object-cover"
-            src={image}
+            src={
+              isDaniel
+                ? daniel
+                : isJeanette
+                ? jeanette
+                : isJonathan
+                ? jonathan
+                : isKira
+                ? kira
+                : isPatrick
+                ? patrick
+                : null
+            }
             alt={name}
           />
         </div>
@@ -56,7 +74,6 @@ Testimonial.propTypes = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   role: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   backgroundColor: PropTypes.string.isRequired,
